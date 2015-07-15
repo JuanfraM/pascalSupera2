ee917a3d68104922ba5bfbfb6ef27620f0485735
juanfra <juanfra>
2015-07-15 00:54:17 -0300

    putaso

--- src/com/language/model/expression/FuncionDef.java
+++ src/com/language/model/expression/FuncionDef.java
@@ -1,17 +1,32 @@
 package com.language.model.expression;
 
 import java.util.ArrayList;
+import java.util.Map;
 
-public class FuncionDef {
+import com.language.Scope;
+
+public class FuncionDef extends Sentencia {
 	
-	private String valor;
+	private Object valor;
 	private ArrayList<String> parametros;
 	private ArrayList<Sentencia> sentencias;
 	
-	public FuncionDef (String valor, ArrayList<String> parametros, ArrayList<Sentencia> sentencias){
+	public FuncionDef (Object valor, ArrayList<String> parametros, ArrayList<Sentencia> sentencias){
 		this.valor = valor;
 		this.parametros = parametros;
 		this.sentencias = sentencias;
+	}
+
+	@Override
+	public Resultado ejecutar(Scope variables, Map<String, FuncionDef> Funciones) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getValor() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
