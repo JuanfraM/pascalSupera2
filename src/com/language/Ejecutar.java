21c5fd94f8c04388b9ba3c32b8c71f91d34d7194
juanfra <juanfra>
2015-07-15 03:02:51 -0300

    a

--- src/com/language/Ejecutar.java
+++ src/com/language/Ejecutar.java
@@ -6,8 +6,10 @@ import java.util.Map;
 import com.language.model.expression.FuncionCall;
 import com.language.model.expression.FuncionDef;
 import com.language.model.expression.FuncionesPredefinidas;
+import com.language.model.expression.If;
 import com.language.model.expression.Sentencia;
 import com.language.model.expression.Variable;
+import com.language.model.expression.While;
 
 public class Ejecutar {
 	
@@ -32,6 +34,12 @@ public class Ejecutar {
 			
 			//Funciones del usuario
 			else if (stmt instanceof FuncionCall){
+				stmt.ejecutar(Variables, Funciones);
+			}
+			else if (stmt instanceof If){
+				stmt.ejecutar(Variables, Funciones);
+			}
+			else if (stmt instanceof While){
 				stmt.ejecutar(Variables, Funciones);
 			}
 		}
