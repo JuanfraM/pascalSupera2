26fa26cd83db3923f2d967e43c26a8fd531ab247
sebastian.lorenzo <sebastianlorenzo88@gmail.com>
2015-07-16 22:51:15 -0300

    -

--- src/com/language/model/expression/Return.java
+++ src/com/language/model/expression/Return.java
@@ -29,6 +29,8 @@ public class Return extends Sentencia {
 	public Resultado ejecutar(Scope variables, Map<String, FuncionDef> Funciones, boolean loop) {
 		if (this.arguments.isEmpty())
 			return new Resultado ("None", TipoResultado.NONE);
+		if (this.arguments.size() == 1)
+			return this.arguments.get(0).ejecutar(variables, Funciones, loop);
 		ArrayList <Resultado> a = new ArrayList <Resultado>();
 		for (Expresion e : this.arguments){
 			a.add(e.ejecutar(variables, Funciones, loop));
